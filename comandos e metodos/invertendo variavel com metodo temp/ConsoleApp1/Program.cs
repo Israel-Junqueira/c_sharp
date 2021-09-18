a61f310e8b75dbcffbb6e26c1843d426211cbd4e
Israel-Junqueira <israelribeiro313@gmail.com>
2021-09-18 01:08:57 -0300

    adicionei o comando temp e organizei as pastas

---
+++ comandos e metodos/invertendo variavel com metodo temp/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x, y;
+            x = 2;
+            y = 3;
+
+            int temp = x;
+            x = y;
+            y = temp;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            */
+        }
+    }
+}
